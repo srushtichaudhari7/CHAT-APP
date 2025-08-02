249bfafbbe51b5aab68719e98202edee19f82735
srushtichaudhari7 <srushtichaudhari43@gmail.com>
2025-08-03 04:29:07 +0530

    authentication

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,83 @@
-export const signup= (req, res) => {
-    res.send("Signup route");
+import { generateToken } from '../lib/utils.js';
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+
+export const signup= async(req, res) => {
+    const { email, fullName, password, profilePic } = req.body;
+    try{
+        if(!email || !fullName || !password) {
+            return res.status(400).json({ message: "Please fill all the fields" });
+        }
+        if(password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters long" });
+        }
+        const user= await User.findOne({ email });
+        if(user) {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const newUser = new User({ email, fullName, password: hashedPassword, profilePic });
+       if(newUser) {
+          //jwt
+          generateToken(newUser._id, res);
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                email: newUser.email,
+                fullName: newUser.fullName,
+                profilePic: newUser.profilePic,
+                // message: "User created successfully"
+             });
+
+        }
+        else{
+            res.status(400).json({ message: "Invalid User Data" });
+        }
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server Error" });
+    }
 };
-export const login = (req, res) => {
-    res.send("Login route");
+export const login = async(req, res) => {
+    const { email, password } = req.body;
+    if(!email || !password) {   
+        return res.status(400).json({ message: "Please fill all the fields" });
+    }   
+    try{
+        const user = await User.findOne({ email });
+        if(!user) {
+            return res.status(400).json({ message: "User not found" });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        generateToken(user._id, res);
+        res.status(200).json({
+            _id: user._id,
+            email: user.email,
+            fullName: user.fullName,
+            profilePic: user.profilePic,
+            // message: "Login successful"
+        });
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server Error" });
+    }
+
 }       ;
 export const logout = (req, res) => {               
-    res.send("Logout route");
-}
+   try{
+    res.clearCookie('jwt');
+    res.status(200).json({ message: "Logout successful" });
+   }
+   catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+   }
+};
+
+export const updateProfile=async(req, res) => {
---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+import user from '../models/user.model.js';
+
+export const protectRoute = async (req, res, next) => { 
+    try{
+        const token = req.cookies.jwt;
+        if(!token) {
+            return res.status(401).json({ message: "Not authorized, no token" });
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if(!decoded) {
+            return res.status(401).json({ message: "Not authorized, token failed" });
+        } 
+        const user = await User.findById(decoded.userId).select("-password");
+        if(!user) {
+            return res.status(401).json({ message: "User not found" });
+        }
+        req.user = user;
+        next();
+    }
+    catch (error) {
+        console.error(error);
+        res.status(401).json({ message: "Not authorized, token failed" });  
+    }
+};
