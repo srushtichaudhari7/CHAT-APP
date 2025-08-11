2a2fc2d8644784d09d3a0b33c27c8a6bc63b2244
srushtichaudhari7 <srushtichaudhari43@gmail.com>
2025-08-11 23:36:37 +0530

    components and pages in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import Navbar from './components/Navbar' // Assuming Navbar is a component in your project
+import { Routes,Route } from 'react-router-dom'
 const App = () => {
   return (
-    <div classname="text-3xl font-bold underline">Hello</div>
+    <div>
+      <Navbar /> 
+    <Routes>
+      <Route  path="/" element={<h1>Home Page</h1>} />
+      <Route  path="/signup" element={<h1>signup Page</h1>} />
+      <Route  path="/login" element={<h1>Login Page</h1>} />
+      <Route  path="/settings" element={<h1>settings Page</h1>} />
+      <Route  path="/profile" element={<h1>profile Page</h1>} />
+    </Routes> 
+    </div>
+    // Assuming navbar is a component that renders the navigation bar 
   )
 }
 
